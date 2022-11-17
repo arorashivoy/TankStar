7f819a1f524705f254c8c78bfaeebf2683d102dc
Shivoy Arora <85881270+arorashivoy@users.noreply.github.com>
2022-11-17 22:36:23 +0530

    Initial commit

---
+++ core/src/com/arorashivoy/tank_star/Main.java
@@ -0,0 +1,30 @@
+package com.arorashivoy.tank_star;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends Game {
+	public SpriteBatch batch;
+	public OrthographicCamera camera;
+	
+	@Override
+	public void create () {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
---
+++ core/src/com/arorashivoy/tank_star/screens/MainMenu.java
@@ -0,0 +1,46 @@
+package com.arorashivoy.tank_star.screens;
+
+import com.arorashivoy.tank_star.Main;
+import com.badlogic.gdx.Screen;
+
+public class MainMenu implements Screen {
+	private Main app;
+	public MainMenu(Main app) {
+		this.app = app;
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
